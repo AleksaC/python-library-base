b936aee973bc59f1ea62acd9eed696580565403e
AleksaC <aleksacukovic1@gmail.com>
2020-09-15 02:03:47 +0200

    Add initial version of the library

--- library/__main__.py
+++ library/__main__.py
@@ -1,7 +1,6 @@
-import sys
+import sys  # pragma: no cover
 
-from .cli import main
-
+from .cli import main  # pragma: no cover
 
 if __name__ == "__main__":
     sys.exit(main())
--- library/cli.py
+++ library/cli.py
@@ -1,2 +1,9 @@
-def main() -> int:
+import argparse
+from typing import Optional, Sequence
+
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args(argv)  # noqa: F841
+
     return 0
